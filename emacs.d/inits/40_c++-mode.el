7c551c6d502d64b409b4d9bae9c2d3857510afdc
LI Tao <tli@techyauld.com>
2017-11-01 13:14:29 +0800

    indent using spaces, set tab width to 2 spaces, Improve indentation of C++11 lambda function

--- emacs.d/inits/40_c++-mode.el
+++ emacs.d/inits/40_c++-mode.el
@@ -4,9 +4,31 @@
 (require 'google-c-style)
 (add-hook 'c++-mode-common-hook 'google-set-c-style)
 
+(defadvice c-lineup-arglist (around my activate)
+  "Improve indentation of continued C++11 lambda function opened as argument."
+  (setq ad-return-value
+        (if (and (equal major-mode 'c++-mode)
+                 (ignore-errors
+                   (save-excursion
+                     (goto-char (c-langelem-pos langelem))
+                     ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+                     ;;   and with unclosed brace.
+                     (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+            0                           ; no additional indent
+          ad-do-it)))                   ; default behavior
+
+;; (add-hook 'c++-mode-hook
+;; 	  '(lambda()
+;; 	     (setq c-basic-offset 8)))
+
 (add-hook 'c++-mode-hook
-	  '(lambda()
-	     (setq c-basic-offset 8)))
+	  '(lambda ()
+	     (setq c-indent-level 2)
+	     (setq c-basic-offset 2)
+	     (setq tab-width 2)
+	     ;; (setq-default tab-width 2)
+	     (setq indent-tabs-mode nil))
+	  )
 
 (add-hook 'c++-mode-common-hook
 	  '(lambda()
